f281c1f2a80a4b92ecb0d8872126fe3a7bdec583
sibusiso nkosi <sbnkosi@c4r2s3.wethinkcode.co.za>
2020-06-26 10:05:28 +0200

    first

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbnkosi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/26 09:28:43 by sbnkosi           #+#    #+#             */
+/*   Updated: 2020/06/26 09:28:51 by sbnkosi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+# define EVEN(nbr) (nbr % 2 == 0)
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+typedef int		t_bool;
+
+#endif
+
